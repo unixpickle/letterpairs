c92b45dadf515e4b40b32098ef2f60d16c230d59
Alex Nichol <unixpickle@gmail.com>
2017-07-10 19:24:44 -0400

    delete entries

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,11 +1,16 @@
 (function() {
 
-  window.letterpairs.setData = (data) => {
+  window.letterpairs.mutateData = (mutator) => {
+    const dataCopy = {};
+    Object.keys(window.letterpairs.data).forEach((key) => {
+      dataCopy[key] = window.letterpairs.data[key].slice();
+    });
+    const newData = mutator(dataCopy);
     const req = new Request('/update', {
       method: 'post',
-      body: JSON.stringify(data)
+      body: JSON.stringify(newData)
     });
-    return fetch(req);
+    return fetch(req).then(() => window.letterpairs.data = newData);
   };
 
 })();
--- assets/js/list.js
+++ assets/js/list.js
@@ -4,14 +4,14 @@
 
   class List {
     constructor(route) {
-      const pair = route.slice(1);
+      this._pair = route.slice(1);
 
       this._container = document.createElement('div');
       this._container.className = 'list-container';
 
-      this._createHeading(pair);
-      this._createList(pair);
-      this._createAddButton(pair);
+      this._createHeading();
+      this._createList();
+      this._createAddButton();
 
       document.body.appendChild(this._container);
 
@@ -28,57 +28,80 @@
       window.removeEventListener('keypress', this._keyListener);
     }
 
-    _createHeading(pair) {
+    _createHeading() {
       this._heading = document.createElement('div');
-      this._heading.appendChild(window.letterpairs.letterHeading(pair[0]));
-      this._heading.appendChild(window.letterpairs.letterHeading(pair[1]));
+      this._heading.appendChild(window.letterpairs.letterHeading(this._pair[0]));
+      this._heading.appendChild(window.letterpairs.letterHeading(this._pair[1]));
       this._container.appendChild(this._heading);
     }
 
-    _createList(pair) {
-      const options = window.letterpairs.data[pair.toUpperCase()];
+    _createList() {
+      const options = window.letterpairs.data[this._pair.toUpperCase()];
       this._list = document.createElement('div');
       options.forEach((option) => {
         const elem = document.createElement('div');
         elem.className = 'option';
-        elem.textContent = option;
+
+        const elemText = document.createElement('label');
+        elemText.textContent = option;
+        elem.appendChild(elemText);
+
+        const elemButton = document.createElement('button');
+        elemButton.className = 'delete-button';
+        elemButton.addEventListener('click', () => {
+          this._deleteOption(option);
+        });
+        elem.appendChild(elemButton);
+
         this._list.appendChild(elem);
       });
       this._container.appendChild(this._list);
     }
 
-    _createAddButton(pair) {
+    _createAddButton() {
       const button = document.createElement('button');
       button.className = 'add-button';
-      button.addEventListener('click', () => {
-        const addition = prompt('Enter puzzle name');
-        if (!addition) {
-          return;
-        }
-
-        let data = window.letterpairs.data;
-        const dupPair = Object.keys(data).find((key) => {
-          return data[key].find((x) => x.toLowerCase() == addition.toLowerCase());
-        });
-        if (dupPair) {
-          alert('That term is already used in ' + dupPair);
-          return
-        }
-
-        // TODO: make a copy of the data to better handle
-        // addition failures.
-
-        var list = window.letterpairs.data[pair.toUpperCase()];
-        list.splice(0, 0, addition);
-
-        window.letterpairs.setData(window.letterpairs.data)
-          .then(() => {
-            window.letterpairs.refresh();
-          }).catch((e) => {
-            alert('Error adding entry: ' + e);
-          })
-      });
+      button.addEventListener('click', () => this._addOption());
       this._container.appendChild(button);
+    }
+
+    _addOption() {
+      const addition = prompt('Enter puzzle name');
+      if (!addition) {
+        return;
+      }
+
+      let data = window.letterpairs.data;
+      const dupPair = Object.keys(data).find((key) => {
+        return data[key].find((x) => x.toLowerCase() == addition.toLowerCase());
+      });
+      if (dupPair) {
+        alert('That term is already used in ' + dupPair);
+        return;
+      }
+
+      this._applyMutator((data) => {
+        var list = data[this._pair.toUpperCase()];
+        list.splice(0, 0, addition);
+        return data;
+      });
+    }
+
+    _deleteOption(option) {
+      this._applyMutator((data) => {
+        var list = data[this._pair.toUpperCase()];
+        list.splice(list.indexOf(option), 1);
+        return data;
+      });
+    }
+
+    _applyMutator(mutator) {
+      window.letterpairs.mutateData(mutator)
+        .then(() => {
+          window.letterpairs.refresh();
+        }).catch((e) => {
+          alert('Error updating: ' + e);
+        });
     }
   }
 
