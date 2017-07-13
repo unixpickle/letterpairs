e3cf499dde70190c7e67b70c6067697019aa67fa
Alex Nichol <unixpickle@gmail.com>
2017-07-13 18:08:27 -0400

    changed around some colors

--- assets/js/letters.js
+++ assets/js/letters.js
@@ -3,11 +3,11 @@
   function letterColor(letter) {
     const rowColors = [
       '#fff',
-      '#ffc000',
-      '#6add50',
-      '#eb5664',
-      '#2158e4',
-      '#dce320',
+      'rgb(255, 143, 44)',
+      'rgb(106, 221, 80)',
+      'rgb(243, 85, 99)',
+      'rgb(25, 163, 242)',
+      'rgb(220, 227, 32)',
     ];
     let idx = window.letterpairs.letters.indexOf(letter);
     if (idx < 0) {
